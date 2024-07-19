25c2e5f2f93e834b2c928a13f0b4c2e75a7d279b
Alex Ioannides <5968486+AlexIoannides@users.noreply.github.com>
2024-07-19 11:17:13 +0100

    Add simple CI job (#3)
    Co-authored-by: alexioannides <alex.ioanndies@yahoo.co.uk>

--- environments/prod/variables.tf
+++ environments/prod/variables.tf
@@ -1,3 +1,4 @@
+# Default values are to enable CI tests
 variable "region" {
   description = "AWS region"
   type        = string
@@ -7,20 +8,23 @@ variable "region" {
 variable "namespace" {
   description = "The project namespace to use for unique resource naming"
   type        = string
+  default     = "dev-workstation"
 }
 
 variable "ami" {
   description = "AMI to use for instance (must be compatible with instance)"
   type        = string
+  default     = "ami-0a6d88003a7f81c18"
 }
 
 variable "instance_type" {
   description = "EC2 instance type to use for workstation (must be compatible with AMI)"
   type        = string
+  default     = "p3.2xlarge"
 }
 
 variable "bucket_name" {
   description = "S3 bucket for workstation data I/O"
   type        = string
-  default     = null
+  default     = "ioannides-dev-data"
 }
